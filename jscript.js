b10355417995c435ba7b4a98cb417a6671ce6ce7
harsimran141 <harsimran141@gmail.com>
2026-07-27 22:56:06 -0400

    Refactor event listener logic in To Do App to improve readability and maintainability

--- jscript.js
+++ jscript.js
@@ -3,40 +3,42 @@ const addbtn = document.querySelector(".btn");
 const showdata = document.querySelector(".list");
 
 let editItem = "";
+
 addbtn.addEventListener("click", () => {
-    const inputdata = displayinput.value.trim();
-    if (editItem !== "") {
-        editItem.firstChild.textContent = inputdata;
+  const inputdata = displayinput.value.trim();
 
-        editItem = "";
-        addbtn.textContent = "Add";
-        displayinput.value = "";
+  if (editItem !== "") {
+    editItem.firstChild.textContent = inputdata;
 
-        return;
-    }
-    if (inputdata === "") {
-        return;
-    }
-    const list = document.createElement("li");
-    list.textContent = inputdata;
-    list.classList.add("newlist");
-    showdata.appendChild(list);
+    editItem = "";
+    addbtn.textContent = "Add";
     displayinput.value = "";
-    const Del = document.createElement("button");
-    Del.textContent = "Del";
-    Del.classList.add("Delbtn");
-    list.appendChild(Del);
-    Del.addEventListener("click", () => {
-        list.remove();
-    });
-    const Edit = document.createElement("button");
-    Edit.textContent = "Edit";
-    Edit.classList.add("Editbtn");
-    list.appendChild(Edit);
 
-    Edit.addEventListener("click", () => {
-        displayinput.value = inputdata;
-        editItem = list;
-        addbtn.textContent = "Update";
-    });
+    return;
+  }
+  if (inputdata === "") {
+    return;
+  }
+  const list = document.createElement("li");
+  list.textContent = inputdata;
+  list.classList.add("newlist");
+  showdata.appendChild(list);
+  displayinput.value = "";
+  const Del = document.createElement("button");
+  Del.textContent = "Del";
+  Del.classList.add("Delbtn");
+  list.appendChild(Del);
+  Del.addEventListener("click", () => {
+    list.remove();
+  });
+  const Edit = document.createElement("button");
+  Edit.textContent = "Edit";
+  Edit.classList.add("Editbtn");
+  list.appendChild(Edit);
+
+  Edit.addEventListener("click", () => {
+    displayinput.value = inputdata;
+    editItem = list;
+    addbtn.textContent = "Update";
+  });
 });
